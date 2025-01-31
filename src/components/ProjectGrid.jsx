97f46f73b46e9dfb5225527ba890f9e4073c2a6c
AKherde <124738213+AKherde@users.noreply.github.com>
2025-01-30 20:47:55 -0500

    fixed it a little more

--- src/components/ProjectGrid.jsx
+++ src/components/ProjectGrid.jsx
@@ -4,8 +4,9 @@ import ProjectCard from "./ProjectCard.jsx";
 const ProjectGrid = () => {
     return (
         <div className="container mx-auto px-6 pt-20" id="projects">
-            <h2 className="text-3xl font-bold text-white text-center mb-8">My Projects</h2>
-            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+            {/*<h2 className="text-3xl font-bold text-white text-center mb-8">My Projects</h2>*/}
+            <h3 className="head-text">My Projects</h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 pt-12">
                 {myProjects.map((project, index) => (
                     <ProjectCard key={index} {...project}/>
                 ))}
